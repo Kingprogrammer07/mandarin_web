96eb27023a6cf8ed933ae865f1f0e1542c4ca13f
Kingprogrammer07 <gaybullayevjorabek@gmail.com>
2026-05-07 09:40:46 +0500

    feat: add @vercel/speed-insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.tsx'
 // import eruda from 'eruda';
@@ -33,6 +34,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <App />
         <Analytics />
+        <SpeedInsights />
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
