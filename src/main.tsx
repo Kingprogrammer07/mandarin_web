1a4537a46ade3e2143e2c7600ca4641fcd0694e7
Kingprogrammer07 <gaybullayevjorabek@gmail.com>
2026-05-07 09:34:10 +0500

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
-import eruda from 'eruda';
+// import eruda from 'eruda';
 
-eruda.init();
+// eruda.init();
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -31,6 +32,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
